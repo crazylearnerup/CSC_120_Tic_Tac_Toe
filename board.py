d92ec0081152462969eba4a4ae4a761a1c1ed4cd
janic <bmgusa2012@gmail.com>
2022-07-11 22:21:08 -0400

    'Stage1'

---
+++ board.py
@@ -0,0 +1,12 @@
+def board():
+
+   board = [['-','-','-'],
+            ['-','-','-'],
+            ['-','-','-']]
+
+   for i in board:
+       for j in i:
+         print(j, end = "  ")
+       print()    
+
+board()
