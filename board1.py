345287e1b9c39f8214756d4aa52e68f3286528b8
janic <bmgusa2012@gmail.com>
2022-07-13 13:38:30 -0400

    'stage2'

--- board1.py
+++ board1.py
@@ -8,3 +8,43 @@ def board():
        print()
 
 board()
+
+def check_mark(board, symbol):
+
+    valid_move = False
+    while not valid_move:
+        print(symbol + "'s move")
+        row = int(input("Row: "))
+        col = int(input("Col: "))
+        if row < 0 or row >= 3 or col < 0 or col >= 3:
+            print("Please try again.")
+        else:
+            board[row][col] = symbol
+            valid_move = True
+
+
+def check_row(board, row):
+    symbol = board[row][0]
+    for col in range(1, 3):
+        if board[row][col] != symbol:
+            return None
+    return symbol
+
+
+def check_column(board, col):
+    symbol = board[0][col]
+    for row in range(1, 3):
+        if board[row][col] != symbol:
+            return None
+    return symbol
+
+
+def check_diagonal(board):
+
+    symbol = board[0][0]
+    for row in range(1, 3):
+        if board[row][row] != symbol:
+            return None
+    return symbol
+
+   
